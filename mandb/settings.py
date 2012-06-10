a6aee4dc37095b5655ab03cc0e917e56f4c09346
Nehemiah I. Dacres <vivacarlie@gmail.com>
2012-06-09 22:04:18 -0500

    fixed syntax error

--- mandb/settings.py
+++ mandb/settings.py
@@ -11,7 +11,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')})}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
