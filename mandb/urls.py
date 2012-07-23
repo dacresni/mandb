cff733992233ec49e44840bc783855c8115d2594
Nehemiah I. Dacres <vivacarlie@gmail.com>
2012-07-23 00:42:14 -0500

    changed urls/views to attempt byname with named expressioins

--- mandb/urls.py
+++ mandb/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
-
+from  django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'django.views.generic.simple.direct_to_template', { 'template': 'index.html' } ),
+     url(r'^$',direct_to_template , { 'template': 'index.html' } ),
+     url(r'^index.htm$',direct_to_template , { 'template': 'index.html' } ),
     # url(r'^mandb/', include('mandb.foo.urls')),
-     url('^by_name/(\w+)/$','mandb.views.by_name'),
+     url('^by_name/(?P<name>\w+)/$','mandb.views.by_name'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
