55a2769b473e54cdbd2eb563ba99ec7e37eea16b
Nehemiah I. Dacres <vivacarlie@gmail.com>
2012-07-26 22:05:45 -0500

    changed from appengine to django models

--- mandb/Models.py
+++ mandb/Models.py
@@ -1,27 +1,28 @@
-# models 
-from gooogle.appengine.ext import db
-from gooogle.appengine.api import users
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
 
-class Manual(db.Model):
-    name = db.StringProperty(required=True)
-    date = db.DateProperty()
-    content = db.TextProperty()
-    section = db.IntegerProperty()
-    whatis = db.StringProperty()
-    
-class Function(db.Model):
-    name = db.StringProperty(required=True)
-    #params = db.StringProperty()
-    #I've been considering the utility of a parameter list for a while
-    manual = db.ReferenceProperty(Manual,collection_name="functions")
+from django.db import models
 
+class Function(models.Model):
+    name = models.TextField(primary_key=True)
+    header = models.TextField()
+    class Meta:
+        db_table = u'function'
+
+class Manual(models.Model):
+    name = models.TextField(primary_key=True)
+    content = models.TextField()
+    date = models.DateField()
+    section = models.SmallIntegerField()
+    whatis = models.TextField()
+    class Meta:
+        db_table = u'manual'
 class Header(db.Model):
-    name = db.StringProperty(required = True) 
-    manuals = db.ListProperty(db.Key)
-
-
-
-
-
-
-
+    name = models.TextField(required = True)
+    #manuals = models.
